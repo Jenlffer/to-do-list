61a17481cd1f423d6742d6869d339b3daddffda3
Jeniffer Candido <jenifferoliveiracandido@gmail.com>
2023-12-22 11:34:26 -0300

    deploy config

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDb = () => {
   mongoose
     .connect(
-      "mongodb+srv://root:amem@todolist.hu9upym.mongodb.net/?retryWrites=true&w=majority",
+      process.env.DB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
