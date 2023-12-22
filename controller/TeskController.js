2f029bbf15c5bfdc621335eb37f37609922b619d
Jeniffer Candido <jenifferoliveiracandido@gmail.com>
2023-12-22 09:40:09 -0300

    registrando no bd

--- controller/TeskController.js
+++ controller/TeskController.js
@@ -1,10 +1,21 @@
 const Task = require('../models/Tesk');
 
-//fazendo uma rota
+let message = "";
+let type = ""; 
+
 const getAllTasks = async (req, res) => {
     try{
+      setTimeout(() => {
+        message = "";
+      }, 2000);
         const tasksList = await Task.find();//pega listas do banco de dados
-        return res.render("index", {tasksList, task: null, taskDelete: null});
+        return res.render("index", {
+          tasksList,
+          task: null, 
+          taskDelete: null,
+          message,
+          type
+        });
     }catch (err) {
         res.status(500).send({error : err.message});
     }
@@ -23,7 +34,8 @@ const createTask = async (req, res) => {
   
     try {
       await Task.create(task);
-  
+      message = "Tarefa criada com sucesso"
+      type = "success"
       return res.redirect("/");
     } catch (err) {
       res.status(500).send({ error: err.message });
@@ -37,10 +49,10 @@ const getById = async (req, res) =>{
         const tasksList = await Task.find();
         if(req.params.method == "update"){
             const task = await Task.findOne({ _id: req.params.id});
-            res.render("index", { task, taskDelete: null, tasksList});
+            res.render("index", { task, taskDelete: null, tasksList, message, type});
         }else{
             const taskDelete = await Task.findOne({ _id: req.params.id});
-            res.render("index", { task: null, taskDelete, tasksList})
+            res.render("index", { task: null, taskDelete, tasksList,  message, type})
         }
     }catch (err) {
         res.status(500).send({error : err.message})
@@ -52,6 +64,8 @@ const updateOneTask = async (req, res) => {
     try {
       const task = req.body;
       await Task.updateOne({ _id: req.params.id }, task);
+      message = "Tarefa atualizada com sucesso!"
+      type ="success"
       res.redirect("/");
     } catch (err) {
       res.status(500).send({ error: err.message });
@@ -61,13 +75,26 @@ const updateOneTask = async (req, res) => {
 const deleteOneTask = async (req, res) => {
   try{
     await Task.deleteOne({ _id: req.params.id });
+    message = "Tarefa apagada com sucesso!"
+    type ="success"
     res.redirect("/");
   } catch (err){
     res.status(500).send({ error: err.message });
   }
 };
 
+const taskCheck = async (req,res) => {
+  try{
+  const task = await Task.findOne({ _id: req.params.id });
 
+  task.check ?  task.check = false : task.check = true;
+
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/")
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+}
 
 
 
@@ -77,4 +104,5 @@ module.exports = {
     getById,
     updateOneTask,
     deleteOneTask,
+    taskCheck,
 };//controller metodo da rota
